22e69ecac1591f79bc6abee8fe6d0c69a2d7efc5
otnansirk <iam.otnansirk@gmail.com>
2025-05-19 23:37:02 +0700

    fix : new product arrival - responsive

--- src/components/NewProductArrival.jsx
+++ src/components/NewProductArrival.jsx
@@ -32,7 +32,7 @@ const NewProductArrival = () => {
 
     return (
         <div id="new-product-arrival" className="justify-center bg-green-100">
-            <section className="flex flex-col md:flex-row bg-red-200 mx-auto">
+            <section className="flex flex-col md:flex-row bg-green-100 mx-auto">
                 <div className="bg-green-100 flex-shrink-0 text-white flex items-center justify-center p-10 w-full md:w-[415px] relative">
                     <h2 className="text-3xl md:text-4xl font-semibold leading-snug text-center md:text-left z-10 ml-0 md:ml-14 max-w-[80%]">
                         BARU HADIR DI KURNIA SARI
@@ -44,7 +44,7 @@ const NewProductArrival = () => {
                     <Each
                         data={PRODUCT}
                         render={product => (
-                            <div style={{ backgroundImage: `url('${product.thumbnail}')` }} className={`bg-cover bg-center shadow-md flex-shrink-0 min-w-[342px]  min-h-[515px] md:w-auto relative`}>
+                            <div style={{ backgroundImage: `url('${product.thumbnail}')` }} className={`bg-cover bg-center shadow-md flex-shrink-0 min-w-[300px] md:min-w-[342px]  min-h-[515px] md:w-auto relative`}>
                                 <div className="p-4 absolute bottom-0 text-white bg-gradient-to-t from-slate-900 to-transparent">
                                     <h3 className="font-bold mb-1">{product.title}</h3>
                                     <p className="text-sm mb-4 leading-5">{product.description}</p>
