edfdc8580a418e814ac717feb3b38d5579289905
otnansirk <iam.otnansirk@gmail.com>
2025-05-20 08:27:02 +0700

    fix : delivery data

--- src/components/Delivery.jsx
+++ src/components/Delivery.jsx
@@ -1,4 +1,21 @@
+import { priceFormat } from "./utils/helper"
 import Icon from "./utils/Icon"
+
+const IN_THE_TOWN = {
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    delivery_estimate: "1 Hari",
+    price: {
+        from: 50000,
+        to: 250000
+    },
+    min_order: "10pcs"
+}
+
+const OUT_OF_TOWN = {
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    delivery_estimate: "1 Hari - 3 Hari kerja",
+    warranty: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+}
 
 const Delivery = () => {
     const onShowOrderGuide = () => {
@@ -22,14 +39,14 @@ const Delivery = () => {
                 </div>
                 <div className="border-b border-gray-200 pb-12 mt-8">
                     <h2 className="font-semibold text-lg mb-3">Pesan Antar Dalam Kota</h2>
-                    <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p className="text-gray-500">{IN_THE_TOWN.description}</p>
                     <div className="mt-5">
                         <h3 className="text-sm font-semibold mb-1">Estimasi Waktu Pengiriman</h3>
-                        <p className="text-gray-500">1 Hari</p>
+                        <p className="text-gray-500">{IN_THE_TOWN.delivery_estimate}</p>
                         <h3 className="text-sm font-semibold mt-3 mb-1">Biaya Pengiriman</h3>
-                        <p className="text-gray-500">Rp 50.000,- - Rp 250.000,-</p>
+                        <p className="text-gray-500">Rp {priceFormat(IN_THE_TOWN.price.from)},- Rp {priceFormat(IN_THE_TOWN.price.to)},-</p>
                         <h3 className="text-sm font-semibold mt-3 mb-1">Minimal Pemesanan</h3>
-                        <p className="text-gray-500">10pcs</p>
+                        <p className="text-gray-500">{IN_THE_TOWN.min_order}</p>
                     </div>
                 </div>
             </div>
@@ -39,20 +56,20 @@ const Delivery = () => {
                 </div>
                 <div className="mt-8">
                     <h2 className="font-semibold text-lg mb-3">Pesan Antar Luar Kota</h2>
-                    <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p className="text-gray-500">{OUT_OF_TOWN.description}</p>
                     <div className="mt-5">
                         <h3 className="text-sm font-semibold mb-1">Jasa Ekspedisi</h3>
-                        <div className="flex gap-5">
+                        <div className="flex gap-5 my-2">
                             <Icon icon="logo-jnt" className="h-4" />
                             <Icon icon="logo-jne" className="h-5" />
                             <Icon icon="logo-anter-aja" className="h-5" />
                         </div>
 
                         <h3 className="text-sm font-semibold mt-3 mb-1">Estimasi Waktu Pengiriman</h3>
-                        <p className="text-gray-500">1 Hari</p>
+                        <p className="text-gray-500">{OUT_OF_TOWN.delivery_estimate}</p>
 
                         <h3 className="text-sm font-semibold mt-3 mb-1">Garansi Keamanan Produk</h3>
-                        <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p className="text-gray-500">{OUT_OF_TOWN.warranty}</p>
 
                         <a href="#lihat-panduan-pemesanan" onClick={onShowOrderGuide} className="text-red mt-6 inline-block">Lihat Panduan Pengemasan Khusus</a>
                     </div>
